369dc4bb8b0781c5387f99be87c724f7790f9350
Aswin Krishna <aswinkrishna005@gmail.com>
2025-11-07 09:46:36 +0530

    fix

--- src/redux/slice/userSlice.js
+++ src/redux/slice/userSlice.js
@@ -1,23 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name:"user",
-    initialState:{
-        user:null
+  name: "user",
+  initialState: {
+    user: null,
+    pendingCart: null,
+    redirectAfterLogin: null,
+  },
+  reducers: {
+    addUser: (state, action) => {
+      state.user = action.payload;
     },
-    reducers:{
-          
-        addUser:(state,action)=> {
-            state.user = action.payload
-        },
 
-        removeUser:(state,action)=>{
-            state.user = null;
-        },     
-       
-    }
-})
+    removeUser: (state, action) => {
+      state.user = null;
+    },
 
+    setPendingCart: (state, action) => {
+      state.pendingCart = action.payload;
+    },
+    clearPendingCart: (state) => {
+      state.pendingCart = null;
+    },
+      setRedirectAfterLogin: (state, action) => {
+      state.redirectAfterLogin = action.payload; 
+    },
+    clearRedirectAfterLogin: (state) => {
+      state.redirectAfterLogin = null;
+    },
+  },
+});
 
-export const { addUser, removeUser } = userSlice.actions
+export const { addUser, removeUser,setPendingCart,clearPendingCart,setRedirectAfterLogin,clearRedirectAfterLogin } = userSlice.actions;
 export default userSlice.reducer;
