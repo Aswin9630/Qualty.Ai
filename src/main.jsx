39743719eb73ca8f42dd107b3bd7b040b4c531d5
Aswin Krishna <aswinkrishna005@gmail.com>
2025-12-27 11:26:12 +0530

    some changes in google tag

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import App from "./App.jsx";
 import { UserProvider } from "./context/userContext.jsx";
 import { Provider } from "react-redux";
 import appStore from "./redux/store.js";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <HelmetProvider>
     <Provider store={appStore}>
       <UserProvider>
         <App />
       </UserProvider>
     </Provider>
+    </HelmetProvider>
   </StrictMode>
 );
